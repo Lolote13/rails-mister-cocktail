5619accdb92dd780721c71387f0ea7e477f586ce
laurie galeron <laurie.galeron@gmail.com>
2018-02-15 15:01:15 +0100

    models cocktail ingredient dose

---
+++ app/models/ingredient.rb
@@ -0,0 +1,8 @@
+class Ingredient < ApplicationRecord
+
+  has_many :doses
+  has_many :cocktails, through: :doses
+  accepts_nested_attributes_for :doses
+
+  validates :name, uniqueness: true, presence: true
+end
